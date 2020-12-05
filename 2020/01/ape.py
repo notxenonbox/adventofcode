98ae16ffeff70920c2ca432d74b958607c3e4f6e
Balázs Vadász <balazsadventures@gmail.com>
2020-12-05 12:58:18 +0100

    Clean up some stuff

--- 2020/01/ape.py
+++ 2020/01/ape.py
@@ -6,19 +6,17 @@ with open('input.txt') as f:
 to_solve = list(filter(None, to_solve.split('\n')))
 to_solve = list(map(int, to_solve))
 
-print('Part 1', '-' * 15)
-
+part1 = None
 for i in to_solve:
 	for j in to_solve:
 		if i + j == 2020:
-			print(f'{i} + {j} = 2020')
-			print(f'{i} * {j} = {i * j}')
+			part1 = i * j
+print(f'part 1: {part1}')
 
-print('Part 2', '-' * 15)
-
+part2 = None
 for i in to_solve:
 	for j in to_solve:
 		for k in to_solve:
 			if i + j + k == 2020:
-				print(f'{i} + {j} + {k} = 2020')
-				print(f'{i} * {j} * {k} = {i * j * k}')
+				part2 = i * j * k
+print(f'part 2: {part2}')
